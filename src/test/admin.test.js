410e4d03aaa4942d5ae6e9d287e36f08766e8181
dchima <dcnnad@gmail.com>
2020-03-29 01:39:29 +0100

    :star: fix coveralls lcov path

--- src/test/admin.test.js
+++ src/test/admin.test.js
@@ -30,4 +30,22 @@ describe('Admin Signup', () => {
     expect(response.body.data.signup.firstName).to.be.a('string');
     expect(response.body.data.signup.lastName).to.be.a('string');
   });
+  it('should fail if user is already signed up', async () => {
+    const response = await chai
+      .request(server)
+      .post('/graphql')
+      .send({
+        query: `mutation { 
+        signup(username: "dan", email: "dan@test.com", password: "password", firstName: "Daniel", lastName: "Fisher", adminKey: "${ADMIN_KEY}") {
+          id
+          username
+          email
+          firstName
+          lastName
+        }
+      }`
+      });
+
+    expect(response.body.errors).to.be.a('array');
+  });
 });
