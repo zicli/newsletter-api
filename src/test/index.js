d3a8081424178fbe693aa0cf620c03dab9249d22
dchima <dcnnad@gmail.com>
2020-03-29 01:25:51 +0100

    :star: remove coverage from circleci

---
+++ src/test/index.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Initial test', () => {
+  it('should be equal to newsletter', () => {
+    expect('newsletter').to.equal('newsletter');
+  });
+});
